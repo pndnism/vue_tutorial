6cbf9a2ee535112f2479c06edfb6f8b11b9e5d6d
pndnism <p.dai.man8@gmail.com>
2021-11-03 22:47:13 +0900

    install vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,126 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-data-table
+    v-model="selected"
+    dense
+    :headers="headers"
+    :items="horseData"
+    :single-select="singleSelect"
+    item-key="horseNumber"
+    :items-per-page="20"
+    show-select
+    class="elevation-1"
+  >
+    <template v-slot:top>
+        <v-switch
+          v-model="singleSelect"
+          label="Single select"
+          class="pa-3"
+        ></v-switch>
+    </template>
+  </v-data-table>
+  
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+  export default {
+    data () {
+      return {
+        singleSelect: false,
+        selected: [],
+        headers: [
+          {
+            text: '馬番',
+            align: 'start',
+            sortable: false,
+            value: 'horseNumber',
+          },
+          { text: '馬名', value: 'horseName' },
+          { text: '単勝オッズ', value: 'tanshoOdds' }
+        ],
+        horseData: [
+          {
+            horseNumber: 1,
+            horseName: 'コントレイル',
+            tanshoOdds: 2.5
+          },
+          {
+            horseNumber: 2,
+            horseName: 'カデナ',
+            tanshoOdds: 257.8
+          },          
+          {
+            horseNumber: 3,
+            horseName: 'モズベッロ',
+            tanshoOdds: 104.6
+          },          
+          {
+            horseNumber: 4,
+            horseName: 'ポタジェ',
+            tanshoOdds: 23.3
+          },          
+          {
+            horseNumber: 5,
+            horseName: 'エフフォーリア',
+            tanshoOdds: 3.4
+          },          
+          {
+            horseNumber: 6,
+            horseName: 'トーセンスーリヤ',
+            tanshoOdds: 67.1
+          },
+          {
+            horseNumber: 7,
+            horseName: 'ワールドプレミア',
+            tanshoOdds: 30.2
+          },
+          {
+            horseNumber: 8,
+            horseName: 'サンレイポケット',
+            tanshoOdds: 113.5
+          },
+          {
+            horseNumber: 9,
+            horseName: 'グランアレグリア',
+            tanshoOdds: 2.8
+          },
+          {
+            horseNumber: 10,
+            horseName: 'カイザーミノル',
+            tanshoOdds: 67.1
+          },
+          {
+            horseNumber: 11,
+            horseName: 'ムイトオブリガード',
+            tanshoOdds: 332.6
+          },
+          {
+            horseNumber: 12,
+            horseName: 'ラストドラフト',
+            tanshoOdds: 190.4
+          },
+          {
+            horseNumber: 13,
+            horseName: 'ペルシアンナイト',
+            tanshoOdds: 159.1
+          },
+          {
+            horseNumber: 14,
+            horseName: 'カレンブーケドール',
+            tanshoOdds: 19.6
+          },
+          {
+            horseNumber: 15,
+            horseName: 'ヒシイグアス',
+            tanshoOdds: 40.4
+          },
+          {
+            horseNumber: 16,
+            horseName: 'ユーキャンスマイル',
+            tanshoOdds: 153.0
+          },
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+        ],
+      }
+    },
+  }
+</script>
---
+++ src/views/Main.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="main">
+        <h1>2021-10-31 天皇賞（秋）</h1>
+        <table class="base-info-table">
+            <tr>
+                <th>馬名</th>
+                <th>単勝オッズ</th>
+            </tr>
+        </table>
+    </div>
+</template>
